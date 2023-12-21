adb88280f48da7f0caa4bf7569ee517ef266d55c
cat2koban <cat2koban@users.noreply.github.com>
2023-12-21 14:42:43 +0900

    5.2.4.2 N:N connection

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -14,6 +14,7 @@ const typeDefs = `
     name: String
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
 
   type Photo {
@@ -23,6 +24,7 @@ const typeDefs = `
     description: String
     category: PhotoCategory!
     postedBy: User!
+    taggedUsers: [User!]!
   }
 
   input PostPhotoInput {
@@ -70,6 +72,12 @@ var photos = [
     "githubUser": "sSchmidt"
   }
 ]
+var tags = [
+  { "photoID": "1", "userID":"gPlake" },
+  { "photoID": "2", "userID":"sSchmidt" },
+  { "photoID": "2", "userID":"mHattrup" },
+  { "photoID": "2", "userID":"gPlake" }
+]  
 
 const resolvers = {
   Query: {
@@ -92,12 +100,20 @@ const resolvers = {
     url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
     postedBy: parent => {
       return users.find(u => u.githubLogin === parent.githubUser)
-    }
+    },
+    taggedUsers: parent => tags
+      .filter(tag => tag.photoID === parent.id)
+      .map(tag => tag.userID)
+      .map(userID => users.find(u => u.githubLogin === userID))
   },
   User: {
     postedPhotos: parent => {
       return photos.filter(p => p.githubUser === parent.githubLogin)
-    }
+    },
+    inPhotos: parent => tags
+      .filter(tag => tag.userID === parent.id)
+      .map(tag => tag.photoID)
+      .map(photoID => photos.find(p => p.id === photoID))
   }
 }
 
