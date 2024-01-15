18fbaf8bc0fac03cd4f26f869df493ac2449de3e
cat2koban <cat2koban@users.noreply.github.com>
2024-01-15 18:05:03 +0900

    wip

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -1,6 +1,9 @@
 const { ApolloServer } = require(`apollo-server`)
+const { GraphQLScalarType } = require(`graphql`)
 
 const typeDefs = `
+  scalar DateTime
+
   enum PhotoCategory {
     SELFIE
     PORTRAIT
@@ -25,6 +28,7 @@ const typeDefs = `
     category: PhotoCategory!
     postedBy: User!
     taggedUsers: [User!]!
+    created: DateTime!
   }
 
   input PostPhotoInput {
@@ -35,7 +39,7 @@ const typeDefs = `
 
   type Query {
     totalPhotos: Int!
-    allPhotos: [Photo!]!
+    allPhotos(after: DateTime): [Photo!]!
     allUsers: [User!]!
   }
 
@@ -56,20 +60,23 @@ var photos = [
     "name": "Dropping the Heart Chute",
     "description": "The heart chute is one of my favorite chutes",
     "category": "ACTION",
-    "githubUser": "gPlake"
+    "githubUser": "gPlake",
+    "created": "3-28-1977"
   },
   {
     "id": "2",
     "name": "Enjoying the sunshine",
     "category": "SELFIE",
-    "githubUser": "sSchmidt"
+    "githubUser": "sSchmidt",
+    "created": "1-2-1985"
   },
   {
     "id": "3",
     "name": "Gunbarrel 25",
     "description": "25 laps on gunbarrel today",
     "category": "LANDSCAPE",
-    "githubUser": "sSchmidt"
+    "githubUser": "sSchmidt",
+    "created": "2018-04-15T19:09:57.308Z"
   }
 ]
 var tags = [
@@ -114,7 +121,14 @@ const resolvers = {
       .filter(tag => tag.userID === parent.id)
       .map(tag => tag.photoID)
       .map(photoID => photos.find(p => p.id === photoID))
-  }
+  },
+  DateTime: new GraphQLScalarType({
+    name: `DateTime`,
+    description: `A valid date time value`,
+    parseValue: value => new Date(value),
+    serialize: value => new Date(value).toISOString(),
+    parseLiteral: ast => ast.value
+  })
 }
 
 const server = new ApolloServer({
